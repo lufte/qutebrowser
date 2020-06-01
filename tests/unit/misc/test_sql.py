0a71f01708280e47b040548db45af2dfcd617d9c
lufte <javier@lufte.net>
2020-06-01 20:08:25 -0300

    Use the same test vchimishuk wrote

--- tests/unit/misc/test_sql.py
+++ tests/unit/misc/test_sql.py
@@ -148,6 +148,24 @@ def test_insert_batch_replace(qtbot):
                             'lucky': [True, True]})
 
 
+def test_update(qtbot):
+    table = sql.SqlTable('Foo', ['a', 'b', 'c'])
+    table.insert({'a': 10, 'b': 10, 'c': 10})
+    table.insert({'a': 20, 'b': 20, 'c': 20})
+
+    with qtbot.waitSignal(table.changed):
+        table.update({'a': 11, 'b': 12, 'c': 13}, {'a': 10, 'b': 10})
+    with qtbot.waitSignal(table.changed):
+        table.update({'a': 21}, {'c': 20})
+
+    assert list(table) == [(11, 12, 13), (21, 20, 20)]
+
+    with qtbot.waitSignal(table.changed):
+        table.update({'a': 'a * a', 'b': 'b + 3'}, {'a': 11, 'b': 12}, False)
+
+    assert list(table) == [(121, 15, 13), (21, 20, 20)]
+
+
 def test_iter():
     table = sql.SqlTable('Foo', ['name', 'val', 'lucky'])
     table.insert({'name': 'one', 'val': 1, 'lucky': False})
