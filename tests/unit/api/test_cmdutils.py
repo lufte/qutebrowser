c31b93f9a710c8aa93e60e92d90b78e0bb58e393
lufte <javier@lufte.net>
2020-10-04 00:37:27 -0300

    Automatic update of typing imports in tests

--- tests/unit/api/test_cmdutils.py
+++ tests/unit/api/test_cmdutils.py
@@ -24,7 +24,7 @@
 import sys
 import logging
 import types
-import typing
+from typing import Union
 import enum
 
 import pytest
@@ -297,17 +297,17 @@ class TestRegister:
         (int, 'x', None, cmdexc.ArgumentTypeError),
         (str, 'foo', None, 'foo'),
 
-        (typing.Union[str, int], 'foo', None, 'foo'),
-        (typing.Union[str, int], '42', None, 42),
+        (Union[str, int], 'foo', None, 'foo'),
+        (Union[str, int], '42', None, 42),
 
         # Choices
         (str, 'foo', ['foo'], 'foo'),
         (str, 'bar', ['foo'], cmdexc.ArgumentTypeError),
 
         # Choices with Union: only checked when it's a str
-        (typing.Union[str, int], 'foo', ['foo'], 'foo'),
-        (typing.Union[str, int], 'bar', ['foo'], cmdexc.ArgumentTypeError),
-        (typing.Union[str, int], '42', ['foo'], 42),
+        (Union[str, int], 'foo', ['foo'], 'foo'),
+        (Union[str, int], 'bar', ['foo'], cmdexc.ArgumentTypeError),
+        (Union[str, int], '42', ['foo'], 42),
 
         (Enum, 'x', None, Enum.x),
         (Enum, 'z', None, cmdexc.ArgumentTypeError),
